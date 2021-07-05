02c7b1f1004b022f8324716b9f2b477e950bfa71
Ivan-Tashev <anl@abv.bg>
2021-07-05 15:21:12 +0300

    Entity classes User, Account added.

---
+++ src/main/java/com/example/springdata/entity/Account.java
@@ -0,0 +1,28 @@
+package com.example.springdata.entity;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "accounts")
+@Data // Lombok
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    @NotNull
+    private BigDecimal balance;
+    @ManyToOne
+    @ToString.Exclude // prevent infinity loop
+    private User user;
+}
---
+++ src/main/java/com/example/springdata/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.springdata.entity;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data // Lombok
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    @NotNull
+    private String username;
+    @Column(nullable = false)
+    private int age;
+    @OneToMany(mappedBy = "user")
+    private Set<Account> accounts = new HashSet<>();
+}
